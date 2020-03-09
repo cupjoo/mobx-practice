3784edd87c0f4fa6eb033cd43fae12f81e84cb87
cupjoo <cupjoo@naver.com>
2020-03-09 22:34:24 +0900

    optimize rendering comp

--- src/components/BasketItemList.js
+++ src/components/BasketItemList.js
@@ -2,29 +2,20 @@ import React from 'react';
 import BasketItem from './BasketItem';
 import { inject, observer } from 'mobx-react';
 
-const BasketItemList = ({ items, total, onTake }) => {
+const BasketItemList = ({ items, onTake }) => {
   const itemList = items.map(item => (
     <BasketItem
-      name={item.name}
-      price={item.price}
-      count={item.count}
+      item={item}
       key={item.name}
       onTake={onTake}
     />
   ));
   return (
-    <div>
-      {itemList}
-      <hr />
-      <p>
-        <b>총합: </b> {total}원
-      </p>
-    </div>
+    <div>{itemList}</div>
   );
 };
 
 export default inject(({ market }) => ({
   items: market.selectedItems,
-  total: market.total,
   onTake: market.take,
 }))(observer(BasketItemList));
--- src/components/SuperMarket.js
+++ src/components/SuperMarket.js
@@ -2,12 +2,14 @@ import React from 'react'
 import SuperMarketTemplate from './SuperMarketTemplate';
 import ShopItemList from './ShopItemList';
 import BasketItemList from './BasketItemList';
+import TotalPrice from './TotalPrice';
 
 const SuperMarket = () => {
   return (
     <SuperMarketTemplate 
       items={<ShopItemList/>} 
-      basket={<BasketItemList/>} 
+      basket={<BasketItemList/>}
+      total={<TotalPrice/>}
     />
   );
 };
